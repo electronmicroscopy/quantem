c54d7d304b9855176646b9f38ca4799d24ed9577
ceobrero <ceobrero@stanford.edu>
2025-10-17 11:32:40 -0700

    Update time_series.py

--- src/quantem/imaging/time_series.py
+++ src/quantem/imaging/time_series.py
@@ -19,10 +19,23 @@ class TimeSeries(Dataset3d):
     def pad_images(
         self,
         pad_width: Union[int, tuple[int, int], None],
-        pad_val = None,
+        pad_val: [str , None] = None,
         edge_blend: Union[int, tuple[int, int], None] = (8,8),
         modify_in_place: bool = False,
     ):
+
+        func_map = {
+            'mean': np.mean,
+            'median': np.median,
+            'max': np.max,
+            'min': np.min
+        }
+
+        if pad_val in func_map:
+            pad_val = func_map[pad_val](self.array)
+        elif pad_val is None:
+            pad_val = 0  
+        
         # padding values
         if pad_width is None:
             pad_width = np.zeros(2,dtype = 'int')
@@ -69,43 +82,45 @@ class TimeSeries(Dataset3d):
                 pad_width[0]:stack_pad.shape[1]-pad_width[0], 
                 pad_width[1]:stack_pad.shape[2]-pad_width[1]] = \
                 self.array 
+        
+        print(stack_pad.shape)
 
+        if edge_blend[0] < 0 or edge_blend[0] > stack_pad.shape[1] / 2 or edge_blend[1] < 0 or edge_blend[1] > stack_pad.shape[2] / 2:
+            raise ValueError("edge_blend is outside of allowable range.")
+        
         return stack_pad
 
-# cap edge blending to 0-half-width (stack_pad.shape)
+        # cap edge blending to 0-half-width (stack_pad.shape)
+
+
+        #pad val that is mean, median, min, and max (take in strings)
+        # None | str
 
 
 
 
-
-
-
-        # print(self.shape, window.shape)
-
-
-        # stack_pad = pad_val * np.ones(s_pad.shape)
-
-        # print(stack_pad.shape)
-
-        # # blend edge
-        # wx = tukey(s_pad.shape[1],alpha=2*edge_blend[0]/s_pad.shape[1])[None,:,None]
-        # wy = tukey(s_pad.shape[2],alpha=2*edge_blend[1]/s_pad.shape[2])[None,None,:]
-        # window = wx * wy
-   
-        # print(wx.shape, wy.shape)
-        # print(window)
-
+    def align_images(
+        self,
+        im_ref,
+        im,
+        # return_aligned_image=True,
+        # window_size=7,
+    ):
+        print(1)
         
+        f_fft = np.fft.fft2(im_ref.array)
 
+        print(2)
+        
+        g_fft = np.fft.fft2(im.array)
 
+        print(3)
+        
+        im_corr = np.real(np.fft.ifft2(f_fft * np.conj(g_fft)))
 
-        # shift to center
+        print(4)
 
-
-#eventually center the image.... 
-
-
-
+        return im_corr
 
 
 
