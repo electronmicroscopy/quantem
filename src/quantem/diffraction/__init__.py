698c8523cf9c3edfd2f16e83125746f1323cd59b
Karen Ehrhardt <ehrhardtkm@gmail.com>
2025-12-16 12:55:23 -0800

    initial pdf code + f parameters

--- src/quantem/diffraction/__init__.py
+++ src/quantem/diffraction/__init__.py
@@ -1,6 +1,4 @@
 from quantem.diffraction.polar import RDF as RDF
-
-from quantem.diffraction.polar import RDF as RDF
 from quantem.diffraction.strain_autocorrelation import (
     StrainMapAutocorrelation as StrainMapAutocorrelation,
 )
