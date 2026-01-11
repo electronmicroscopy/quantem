cb3bc06ddf48b8602aa3da76be5cacdc465a07da
Sangjoon Bob Lee <bobleesj@stanford.edu>
2026-01-10 22:52:53 -0800

    Add quantem.widget namespace package structure

---
+++ widget/src/quantem/widget/__init__.py
@@ -0,0 +1,8 @@
+from importlib.metadata import version
+
+__version__ = version("quantem.widget")
+
+
+def show4dstem():
+    # TODO: Implement 4D-STEM visualization widget
+    print("show4dstem: not yet implemented")
