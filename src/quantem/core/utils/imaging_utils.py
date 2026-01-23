d191e6f24d5506724b8f27a2527da231b9d4a84f
ceobrero <ceobrero@stanford.edu>
2026-01-23 10:30:37 -0800

    Revert "In situ time series alignment class"

--- src/quantem/core/utils/imaging_utils.py
+++ src/quantem/core/utils/imaging_utils.py
@@ -7,7 +7,6 @@ import numpy as np
 import torch
 from numpy.typing import NDArray
 from scipy.ndimage import gaussian_filter
-from scipy.signal import convolve2d
 
 from quantem.core.utils.utils import generate_batches
 
@@ -549,52 +548,6 @@ def fourier_cropping(
     return result
 
 
-def edge_filter(
-    im: NDArray,
-    sigma_edge: int,
-    sobel_filter_val: int | tuple[int,int],
-    ):
-    """
-    Used to filter an image array to define and highlight edges using 2D convolution.
-    
-    Parameters
-    --------
-    im: NDArray
-        Input 2D image array.
-    sigma_edge: int
-        Standard deviation (sigma) of the 1D Gaussian kernel.
-    sobel_filter_val: int | tuple[int,int]
-        Value for the Sobel filter gradient kernel coefficient(s).
-
-    Returns
-    -------
-    im_edge: NDArray
-        2D array containing the gradient magnitude (edge strength) of the input image.
-    """
-
-    sobel_filter_val = np.array(sobel_filter_val)
-    if sobel_filter_val.size == 1:
-        sobel_filter_val = np.array([sobel_filter_val, sobel_filter_val])
-
-    r = np.arange(-np.ceil(2.0*sigma_edge), np.ceil(2.0*sigma_edge+1))
-
-    k = np.exp((r[:,None]**2) / (-2*sigma_edge**2))
-
-    sf = np.array([[-sobel_filter_val[0],0,sobel_filter_val[1]]])
-
-    im_x = convolve2d(im, sf, mode='same', boundary='symm')
-    im_x = convolve2d(im_x, k, mode='same', boundary='symm')
-    im_x = convolve2d(im_x, k.T, mode='same', boundary='symm')
-
-    im_y = convolve2d(im, sf.T, mode='same', boundary='symm')
-    im_y = convolve2d(im_y, k, mode='same', boundary='symm')
-    im_y = convolve2d(im_y, k.T, mode='same', boundary='symm')
-
-    im_edge = np.sqrt(im_x**2 + im_y**2)
-
-    return im_edge
-  
-  
 def compute_fsc_from_halfsets(
     halfset_recons: list[torch.Tensor],
     sampling: tuple[float, float],
--- src/quantem/imaging/__init__.py
+++ src/quantem/imaging/__init__.py
@@ -1,2 +1,1 @@
 from quantem.imaging.drift import DriftCorrection as DriftCorrection
-from quantem.imaging.time_series import TimeSeries as TimeSeries
